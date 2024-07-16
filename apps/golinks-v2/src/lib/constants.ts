63e24f77847510bfa7f18581e5122e3d9b9b5b00
Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>
2024-07-17 00:59:42 +0800

    chore(golinks): add constants
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>

---
+++ apps/golinks-v2/src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const adminApiKey = {
+		type: 'apiKey',
+		name: 'X-Golinks-Admin-Key',
+		in: 'header',
+};
+
+export const homepage = "https://wiki.andreijiroh.xyz/golinks"
+
+export const contact = {
+	name: "Andrei Jiroh Halili",
+	url: homepage,
+	email: "ajhalili2006@andreijiroh.xyz"
+}
